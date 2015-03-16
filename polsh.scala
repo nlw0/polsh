ecd48b7dea58f0032b603e6efec04cdbab2ec2b7
nlw0 <nwerneck@gmail.com>
2015-03-16 13:52:56 -0300

    Lots of refactoring... recursive runOp definition, PolshOperation trait, etc

--- polsh.scala
+++ polsh.scala
@@ -1,69 +1,54 @@
 import scala.io.Source
+import scala.language.implicitConversions
 
+trait PolshOperation
+case class Op2Arg(f: String=>String=>PolshCpu=>PolshCpu) extends PolshOperation
+case class Op1Arg(f: String=>PolshCpu=>PolshCpu) extends PolshOperation
+case class Op0Arg(f: PolshCpu=>PolshCpu) extends PolshOperation
 
 case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, String]=Map()) {
 
-  def push(in: String): PolshCpu = {
-    PolshCpu(in #:: stack, memory).execute
-  }
-  
-  def push(in: Stream[String]): PolshCpu =
-    in match {
-      case tok #:: ts if (tok != "") => push(tok).push(ts)
-      case _ => this
-    }
+  implicit def iop2arg(f: String=>String=>PolshCpu=>PolshCpu) = Op2Arg(f)
+  implicit def iop1arg(f: String=>PolshCpu=>PolshCpu) = Op1Arg(f)
+  implicit def iop0arg(f: PolshCpu=>PolshCpu) = Op0Arg(f)
 
-  def drop(n: Int) = {
-    PolshCpu(stack.drop(n), memory)
+
+  def verbose = println(stack.toList.reverse); println(memory); println(); this
+
+  def push(in: String): PolshCpu = PolshCpu(in #:: stack, memory).verbose.execute
+
+  def push(in: Stream[String]): PolshCpu = in match {
+    case tok #:: ts if (tok != "") => push(tok).push(ts)
+    case _ => this
   }
 
+  def drop(n: Int) = PolshCpu(stack.drop(n), memory)
   def store(kv: (String, String)) = PolshCpu(stack, memory + kv)
+  def free(k: String) = PolshCpu(stack, memory - k)
 
-  def execute: PolshCpu = {
-    println(stack.toList.reverse)
-    println(memory)
-    println()
-
-    stack match {
-      case s #:: _ if arithmetics.isDefinedAt(s) =>
-        drop(1) run2op opArit(arithmetics(s))
-      case "dup" #:: _ => drop(1) run1op {c=>op1=> c push Stream(op1, op1)}
-      case "store" #:: _ => drop(1) run2op {c=>op1=>op2=> c store (op1 -> op2)}
-      case "load" #:: _ => drop(1) run1op {c=>op1=> c push memory(op1)}
-      case _ => this
-    }
-  }
-  
-  def opArit(f: (Int,Int)=>Int): PolshCpu=>String=>String=>PolshCpu =
-    {c=>op1=>op2=> c push f(op2.toInt, op1.toInt).toString}
-
-  val arithmetics: PartialFunction[String, (Int, Int)=>Int] = {
-    case "+" => (_+_)
-    case "-" => (_-_)
-    case "/" => (_/_)
-    case "*" => (_*_)
+  def execute: PolshCpu = stack match {
+    case "dup" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push Stream(op1, op1))
+    case "store" #:: _ => drop(1) runOp Op2Arg(op1=>op2=>c=> c store (op1 -> op2))
+    case "load" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push memory(op1))
+    case op #:: _ if arith.isDefinedAt(op) => drop(1) runOp opArith(arith(op))
+    case _ => this
   }
 
-  val opsMemo = Set("store", "fetch")
+  def runOp(op: PolshOperation): PolshCpu = (op, stack) match {
+    case (Op2Arg(f), arg #:: _) => drop(1) runOp f(arg)
+    case (Op1Arg(f), arg #:: _) => drop(1) runOp f(arg)
+    case (Op0Arg(f), _) => f(this)
+    case _ => this
+  }
 
-  // def run2op(f: PolshCpu=>String=>String=>PolshCpu): PolshCpu =
-  //   stack match {
-  //     case op1 #:: op2 #:: _ => f(drop(2))(op1)(op2)
-  //     case _ => this
-  //   }
+  def opArith(f: (Int,Int)=>Int) = Op2Arg(op1=>op2=>c=> c push f(op2.toInt, op1.toInt).toString)
 
-  def run2op(f: PolshCpu=>String=>String=>PolshCpu): PolshCpu =
-    stack match {
-      // case op1 #:: op2 #:: _ => f(drop(2))(op1)(op2)
-      case op1 #:: _ => drop(1) run1op {c=>op2=> f(c)(op1)(op2) }
-      case _ => this
-    }
-
-  def run1op(f: PolshCpu=>String=>PolshCpu): PolshCpu =
-    stack match {
-      case op1 #:: _ => f(drop(1))(op1)
-      case _ => this
-    }  
+  val arith: PartialFunction[String,(Int,Int)=>Int] = {
+    case "+" => (_ + _)
+    case "-" => (_ - _)
+    case "/" => (_ / _)
+    case "*" => (_ * _)
+  }
 }
 
 object Polsh extends App {
