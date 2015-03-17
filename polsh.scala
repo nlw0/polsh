2f719f47c2b9df4ecc191fb0afbdf04ea6fd50b9
nlw0 <nwerneck@gmail.com>
2015-03-17 01:05:13 -0300

    implemented saving lists of inputs to memory, by open and close square brackets...

--- polsh.scala
+++ polsh.scala
@@ -6,7 +6,7 @@ case class Op2Arg(f: String=>String=>PolshCpu=>PolshCpu) extends PolshOperation
 case class Op1Arg(f: String=>PolshCpu=>PolshCpu) extends PolshOperation
 case class Op0Arg(f: PolshCpu=>PolshCpu) extends PolshOperation
 
-case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, String]=Map()) {
+case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, Any]=Map(), gulpMode:Boolean=false) {
 
   implicit def iop2arg(f: String=>String=>PolshCpu=>PolshCpu) = Op2Arg(f)
   implicit def iop1arg(f: String=>PolshCpu=>PolshCpu) = Op1Arg(f)
@@ -15,7 +15,8 @@ case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, String]=
 
   def verbose = {println(stack.toList.reverse +" "+ memory); this}
 
-  def push(in: String): PolshCpu = PolshCpu(in #:: stack, memory).verbose.execute
+  def push(in: String): PolshCpu =
+    PolshCpu(in #:: stack, memory, gulpMode=gulpMode).verbose.execute
 
   def push(in: Stream[String]): PolshCpu = in match {
     case tok #:: ts if (tok != "") => push(tok).push(ts)
@@ -26,12 +27,21 @@ case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, String]=
   def store(kv: (String, String)) = PolshCpu(stack, memory + kv)
   def free(k: String) = PolshCpu(stack, memory - k)
 
-  def execute: PolshCpu = stack match {
-    case "dup" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push Stream(op1, op1))
-    case "store" #:: _ => drop(1) runOp Op2Arg(op1=>op2=>c=> c store (op1 -> op2))
-    case "load" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push memory(op1))
-    case op #:: _ if arith.isDefinedAt(op) => drop(1) runOp opArith(arith(op))
-    case _ => this
+  def execute: PolshCpu = if (!gulpMode) {
+    stack match {
+      case "dup" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push Stream(op1, op1))
+      case "swap" #:: _ => drop(1) runOp Op2Arg(op1=>op2=>c=> c push Stream(op1, op2))
+      case "store" #:: _ => drop(1) runOp Op2Arg(op1=>op2=>c=> c store (op1 -> op2))
+      case "load" #:: _ => drop(1) runOp Op1Arg(op1=>c=> c push memory(op1).toString)
+      case "[" #:: _ => setGulpMode
+      case op #:: _ if arith.isDefinedAt(op) => drop(1) runOp opArith(arith(op))
+      case _ => this
+    }
+  } else {
+    stack match {
+      case label #:: "]" #:: _ => drop(2).gulpTokens(List[String](), label)
+      case _ => this
+    }
   }
 
   def runOp(op: PolshOperation): PolshCpu = (op, stack) match {
@@ -49,6 +59,14 @@ case class PolshCpu(stack: Stream[String]=Stream(), memory: Map[String, String]=
     case "/" => (_ / _)
     case "*" => (_ * _)
   }
+
+  def setGulpMode = PolshCpu(stack, memory, gulpMode=true)
+
+  def gulpTokens(acc: List[String], label: String): PolshCpu = stack match {
+    case "[" #:: ss => PolshCpu(ss, memory + (label -> acc))
+    case s #:: ss => PolshCpu(ss, memory).gulpTokens(s :: acc, label)
+  }
+
 }
 
 object Polsh extends App {
